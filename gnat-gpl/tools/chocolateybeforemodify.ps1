07dd7bb9c03ddc3afa38ea9fe57769a2e8fca64d
Maxim Reznik <reznikmm@gmail.com>
2016-10-19 22:24:05 +0300

    Add gnat-gpl spec

---
+++ gnat-gpl/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,2 @@
+﻿# This runs in 0.9.10+ before upgrade and uninstall.
+# Use this file to do things like stop services prior to upgrade or uninstall.
---
+++ gnat-gpl/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'gnat-gpl'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://mirrors.cdn.adacore.com/art/573c8c8aa3f5d77ed6451618'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'gnat-gpl*'
+
+  checksum      = 'fa28b759216fa227891eb0ea5fa125b23eef0304'
+  checksumType  = 'sha1'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
