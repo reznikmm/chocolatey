7711c3fad3cbf2d36fc35be247f1aa6b64e98ae6
Maxim Reznik <reznikmm@gmail.com>
2020-05-21 20:20:49 +0300

    Add GNAT Community 2020 release

--- gnat-gpl/tools/chocolateyinstall.ps1
+++ gnat-gpl/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $packageName  = 'gnat-gpl'
 $fileType     = 'exe'
-$url          = 'https://community.download.adacore.com/v1/c13b2d02d23057d9251bcdc9a073cb932177f016?filename=gnat-community-2019-20190517-x86_64-windows-bin.exe'
+$url          = 'https://community.download.adacore.com/v1/966801764ae160828c97d2c33000e9feb08d4cce?filename=gnat-2020-20200429-x86_64-windows-bin.exe'
 $silentArgs   = "--script $($env:ChocolateyPackageFolder)\tools\install.qs"
-$checksum     = 'c13b2d02d23057d9251bcdc9a073cb932177f016'
+$checksum     = '966801764ae160828c97d2c33000e9feb08d4cce'
 $checksumType = 'sha1'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url -Checksum $checksum -ChecksumType $checksumType
