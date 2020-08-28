8e84e5e20985ac233641ff3feab0f727fcf3ff5d
Maxim Reznik <reznikmm@gmail.com>
2020-08-28 16:00:00 +0300

    Change download URL for GNAT CE 2020.

--- gnat-gpl/tools/chocolateyinstall.ps1
+++ gnat-gpl/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 $packageName  = 'gnat-gpl'
 $fileType     = 'exe'
-$url          = 'https://community.download.adacore.com/v1/966801764ae160828c97d2c33000e9feb08d4cce?filename=gnat-2020-20200429-x86_64-windows-bin.exe'
+$url          = 'https://community.download.adacore.com/v1/85091aafe5cb9463f1bfa20a53b6aa0931bf9fc2?filename=gnat-2020-20200818-x86_64-windows-bin.exe'
 $silentArgs   = "--script $($env:ChocolateyPackageFolder)\tools\install.qs"
-$checksum     = '966801764ae160828c97d2c33000e9feb08d4cce'
+$checksum     = '85091aafe5cb9463f1bfa20a53b6aa0931bf9fc2'
 $checksumType = 'sha1'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url -Checksum $checksum -ChecksumType $checksumType
